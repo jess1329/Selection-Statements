c8872b8f4f96dd2baf98f82b6dcf61671d1b5d4c
Jessica Edmonds <jessica1329b@gmail.com>
2022-03-23 13:55:15 -0400

    exercise 1

---
+++ Selection Statements project/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Selection_Statements_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess the correct number!");
+            
+
+
+
+
+
+            var random = new Random();
+            var number = random.Next(1, 10);
+
+           
+
+
+            
+
+            
+            bool correct = false;
+
+            while (!correct)
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                var guess = int.Parse(Console.ReadLine());
+                if (guess > number)
+                {
+                    Console.WriteLine("Your guess is too high, sorry");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Your guess is too low, sorry");
+                }
+                else
+
+                {
+                    correct = true;
+                    Console.WriteLine("Correct!");
+                }
+            }
+        }
+    } 
+}
